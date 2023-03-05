157ff001b1061fcbd9cd6d5c8136f26e01e3ea64
ahmed medhat <ahmed.medhat1407@gmail.com>
2023-03-05 22:04:25 +0000

    from ipad

--- src/components/productImgGallary/ProductImgGallery.jsx
+++ src/components/productImgGallary/ProductImgGallery.jsx
@@ -1,41 +1,7 @@
 import React from "react";
 import { AiOutlineHeart, AiOutlineShareAlt } from "react-icons/ai";
 import placeHolder from "../../assets/iphone.jpg";
-import placeHolder2 from "../../assets/dotsBanner.jpg";
-import placeHolder3 from "../../assets/devider.png";
-import placeHolder4 from "../../assets/floatIpone.jpg";
 
-const data = [
-  {
-    imgList: [
-      {
-        name: "image",
-        description:
-          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore, cumque.",
-        src: placeHolder,
-      },
-      {
-        name: "image",
-        description:
-          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore, cumque.",
-        src: placeHolder2,
-      },
-      {
-        name: "image",
-        description:
-          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore, cumque.",
-        src: placeHolder3,
-      },
-      {
-        name: "image",
-        description:
-          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore, cumque.",
-        src: placeHolder4,
-      },
-    ],
-  },
-  { viewImg: "" },
-];
 const ProductImgGallery = (props) => {
   return (
     <div className="img-gallery">
@@ -44,7 +10,7 @@ const ProductImgGallery = (props) => {
         {/* {props?.imgs.map((img) => {
           return <img src={img} alt={props.name} key={img.id} />;
         })} */}
-        
+
         <img className="img-gallery__left__item" src={placeHolder} alt="" />
         <img className="img-gallery__left__item" src={placeHolder} alt="" />
         <img className="img-gallery__left__item" src={placeHolder} alt="" />
